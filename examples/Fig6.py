755f7bdecd8ab56d54293846f3767e6240700a9e
twhughes <tylerwhughes91@gmail.com>
2019-02-20 16:10:15 -0800

    fixed the figure 6 generation script

--- examples/Fig6.py
+++ examples/Fig6.py
@@ -9,7 +9,7 @@ from DLA_Control.plots import apply_sublabels
 lambda0 = 2e-6   # free space wavelength (m)
 beta = 1         # speed of electron / c0
 N_pil = 10       # pillers per waveguide
-eta = 0.1        # acceleration efficiency G = eta * E0 (unitless)
+eta = 1          # acceleration efficiency G = eta * E0 (unitless)
 DE = 1e6         # energy gain (eV)
 Ud = 20e-9       # damage threshold of waveguides (J)
 h = 2e-6         # pillar height (m)
@@ -17,34 +17,49 @@ tau = 250e-15    # pulse duration (s)
 c0 = 3e8         # speed of light (m/s)
 e0 = 8.85e-12    # free space permittivity (C/V/m)
 n = 2            # refractive index of waveguides
+zeta = np.pi/2   # peak field ratio (Gaussian)
+eta_s = 0.95     # splitting power transmission efficiency
+eta_b = 0.95     # bending power transmission efficiency
+eta_mzi = 0.25   # power efficiency of MZI mesh
 
-N = lambda DE: (DE**2 * tau * h * n * c0 * e0) / (2 * eta**2 * Ud * lambda0 * beta)
+# number of output waveguides needed to supply DE of energy gain for a splitted structure
+N = lambda DE: 2**(np.log2((zeta * DE**2 * tau * h * n * c0 * e0) / (2 * eta**2 * Ud * lambda0 * beta * N_pil)) / np.log2(2 * eta_s * eta_b) )
+
+# the graident of a splitted structure with energy gain DE
 gradient = lambda DE: DE / length(DE)
+
+# the length of a splitted structure with energy gain DE
 length = lambda DE: N(DE) * N_pil * lambda0 * beta
 
-gradient_direct = eta * np.sqrt((2 * Ud) / (N_pil * tau * lambda0 * beta * h * n * e0 * c0))
+# the graidnet of a direct couped structure
+gradient_direct = eta * np.sqrt((2 * Ud * eta_mzi) / (zeta * tau * N_pil * lambda0 * beta * h * n * e0 * c0))
 
+# range of energies to look at
 NE = 10000
-DE_range = np.logspace(3, 6, NE)
+DE_range = np.logspace(4, 6, NE)
 
 N_list = np.zeros((NE, ))
 gradient_list = np.zeros((NE, ))
 length_list = np.zeros((NE, ))
 
+# compute and store data
 for i, DE in enumerate(DE_range):
     N_list[i] = N(DE)
     gradient_list[i] = gradient(DE)
     length_list[i] = length(DE)
 
-print('\nfor splitting structure, to accomplish {} eV of energy gain'.format(DE_range[-1]))
-print('\tgradient of {} V/m'.format(gradient_list[-1]))
-print('\tN_wg of {}'.format(N_list[-1]))
-print('\tlength of (meters){}\n'.format(length_list[-1]))
+# print some stats for the highest energy
+print('\nfor splitting structure, to accomplish {:.2e} eV of energy gain'.format(DE_range[-1]))
+print('\tgradient of {:.2e} V/m'.format(gradient_list[-1]))
+print('\tN_wg of {:.2e}'.format(N_list[-1]))
+print('\tlength of (meters){:.2e}\n'.format(length_list[-1]))
 
-print('for direct coupling structure, to accomplish {} eV of energy gain'.format(DE_range[-1]))
-print('\tgradient of {} V/m'.format(gradient_direct))
-print('\tN_wg of {}'.format(np.sqrt(N_list[-1])))
-print('\tlength of {} (meters)\n'.format(DE_range[-1] / gradient_direct))
+print('for direct coupling structure, to accomplish {:.2e} eV of energy gain'.format(DE_range[-1]))
+print('\tgradient of {:.2e} V/m'.format(gradient_direct))
+print('\tN_wg of {:.2e}'.format(np.sqrt(N_list[-1])))
+print('\tlength of {:.2e} (meters)\n'.format(DE_range[-1] / gradient_direct))
+
+# make plots for Fig 6
 
 fig, axs = plt.subplots(3, 1, sharex=True, constrained_layout=True)
 (ax1, ax2, ax3) = axs
